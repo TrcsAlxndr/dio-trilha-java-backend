f89ecfe0e2c003ee2a374bd890f733b32d4b02d9
Tarcísio Alexandre <trcsalexandre@gmail.com>
2023-09-15 17:18:40 -0300

    Rename main class

---
+++ src/contaBanco.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class contaBanco {
+    public static void main(String[] args) {
+        
+        int value;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello disgraça");
+
+        value = sc.nextInt();
+
+        System.out.println("Este é o valor " + value);
+        
+        sc.close();
+    }
+}
